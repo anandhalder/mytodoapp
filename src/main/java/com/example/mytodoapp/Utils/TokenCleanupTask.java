7460248544bcb7f9ef798b8e267b64722bfc94ea
anandhalder <j.anandmoy@gmail.com>
2023-06-06 22:58:07 +0530

    updating

--- src/main/java/com/example/mytodoapp/Utils/TokenCleanupTask.java
+++ src/main/java/com/example/mytodoapp/Utils/TokenCleanupTask.java
@@ -1,19 +1,20 @@
 package com.example.mytodoapp.Utils;
 
-import com.example.mytodoapp.repository.BlacklistTokenRepository;
+import com.example.mytodoapp.services.TokenBlacklistService;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TokenCleanupTask {
 
-	private final BlacklistTokenRepository blacklistTokenRepository;
+	private final TokenBlacklistService blacklistTokenRepository;
 
-	public TokenCleanupTask(BlacklistTokenRepository blacklistTokenRepository) {
+	public TokenCleanupTask(TokenBlacklistService blacklistTokenRepository) {
 		this.blacklistTokenRepository = blacklistTokenRepository;
 	}
 
-	//	@Scheduled(fixedRate = 10 * 1000)
+	@Scheduled(cron = "0 * * * * *")
 	public void cleanUpTokens() {
-//		blacklistTokenRepository.deleteByExpiryDateBefore(java.time.LocalDateTime.now());
+		blacklistTokenRepository.deleteByExpiryDateBefore(java.time.LocalDateTime.now());
 	}
 }
