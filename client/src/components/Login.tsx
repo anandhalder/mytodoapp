91b00d799d760988aad882290da6b0f74659f75c
anandhalder <j.anandmoy@gmail.com>
2023-06-19 19:22:11 +0530

    Added Login page and added CSS and Validation.

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -5,14 +5,44 @@ function Login() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 
+	const changeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setUsername(event.target.value);
+	}
+
+	const changePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.target.value);
+	}
+
+	const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		console.log("Username: " + username);
+		console.log("Password: " + password);
+	}
+
+	// TODO: We con improve this validation like minimum length.
+	const isSubmitDisabled = username.trim() === '' || password.trim() === '';
+
 	return (
-		<div className="Login">
-			<form>
-				<input type="text" placeholder="Username" value={username} />
-				<input type="password" placeholder="Password" value={password} />
-				<button type="submit">Login</button>
-			</form>
-		</div>
+		<form onSubmit={submitHandler} className="container">
+			<h1>Login</h1>
+			<div className="form-group">
+				<input type="text"
+							 id="username"
+							 name="username"
+							 placeholder="Enter your username" value={username}
+							 onChange={changeUsername}/>
+			</div>
+			<div className="form-group">
+				<input type="password"
+							 id="password"
+							 name="password"
+							 placeholder="Enter your password"
+							 value={password}
+							 onChange={changePassword}/>
+			</div>
+			<button disabled={isSubmitDisabled} className="btn" type="submit">Login</button>
+		</form>
 	);
 }
 
