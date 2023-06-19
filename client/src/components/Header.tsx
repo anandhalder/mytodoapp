890f929b7e76a0d7691181cf944907c9f60526fe
anandhalder <j.anandmoy@gmail.com>
2023-06-19 19:26:04 +0530

    Added Header Page.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 function Header() {	// This is a component
 	return (
-		<div className="Header">
-			<p>Header</p>
-		</div>
+		<header>
+			<div className="header-container">
+				<div className="logo">Your Logo</div>
+				<nav>
+					<ul className="navigation">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
 	);
 }
 
